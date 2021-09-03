ac20694713abcb6c3913ea5ff659359ea08d9452
Gaurav-Jain-0781 <gauravjain0781@gmail.com>
2021-09-03 22:29:55 +0530

    added the save function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 
 def create_file():
@@ -9,13 +10,42 @@ def create_file():
     notebook.select(text_area)
 
 
+def save_file():
+    file = filedialog.asksaveasfilename()
+    try:
+        file_name = os.path.basename(file)
+        text_widget = root.nametowidget(notebook.select())
+        content = text_widget.get("1.0", "end-1c")
+
+        with open(file, 'w') as file:
+            file.write(content)
+
+    except(AttributeError, FileNotFoundError):
+        print("save command cancelled")
+        return
+
+    notebook.tab("current", text=file_name)
+
+
 root = tk.Tk()
 root.title('notebook')
+root.option_add("*tearOff", False)
 
 main = ttk.Frame(root)
 main.pack(fill="both", expand=True, padx=10, pady=(10, 10))
 
+menubar = tk.Menu()
+root.config(menu=menubar)
+
+file_menu = tk.Menu(menubar)
+menubar.add_cascade(label="File", menu=file_menu)
+
+file_menu.add_command(label="new", command=create_file)
+file_menu.add_command(label="save", command=save_file)
+
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
+
+create_file()
 
 root.mainloop()
