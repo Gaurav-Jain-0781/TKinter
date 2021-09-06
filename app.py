6031cefb3013a812fdb1fd3fae44313cc57f3bec
Gaurav-Jain-0781 <gauravjain0781@gmail.com>
2021-09-06 22:35:48 +0530

    addeda a confirm quit function and save changes functionality.

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
 import os
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
+
+contents = dict()
 
 
 def create_file(content="", filename="Untitled"):
     text_area = tk.Text(notebook)
     text_area.insert("end", content)
+
     text_area.pack(fill="both", expand=True)
     notebook.add(text_area, text=filename)
+
     notebook.select(text_area)
+
+    contents[str(text_area)] = hash(content)
+
+
+def check_to_changes():
+    current = root.nametowidget(notebook.select())
+    content = current.get("1.0", "end-1c")
+    name = notebook.tab("current")['text']
+
+    if hash(content) != contents[str(current)]:
+        if name[-1] != "*":
+            notebook.tab("current", text=name + "*")
+    elif name[-1] == "*":
+        notebook.tab("current", text=name[:-1])
+
+
+def confirm_quiting():
+    unsaved = False
+
+    for tab in notebook.tabs():
+        current = root.nametowidget(tab)
+        content = current.get("1.0", "end-1c")
+
+        if hash(content) != contents[str(current)]:
+            unsaved = True
+            break
+
+    if unsaved == True:
+        confirm = messagebox.askyesno(
+            message="Changes to your file has not been saved . Do you want to quit ?",
+            icon="question",
+            title="Confirm Quit"
+        )
+        if not confirm:
+            return
+
+    root.destroy()
 
 
 def save_file():
@@ -57,14 +98,17 @@ menubar.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="New", command=create_file, accelerator="Ctrl+N")
 file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
 file_menu.add_command(label="Open", command=open_file, accelerator="Ctrl+O")
+file_menu.add_command(label="Quit", command=confirm_quiting, accelerator="Ctrl+Q")
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
 
 create_file()
 
+root.bind("<KeyPress>", lambda event: check_to_changes())
 root.bind("<Control-n>", lambda event: create_file())
 root.bind("<Control-s>", lambda event: save_file())
 root.bind("<Control-o>", lambda event: open_file())
+root.bind("<Control-q>", lambda event: confirm_quiting())
 
 root.mainloop()
