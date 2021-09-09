0c0b869d8f9392d4327512c2d621a7bd43052677
Gaurav-Jain-0781 <gauravjain0781@gmail.com>
2021-09-09 22:19:51 +0530

    added quit function and info function

--- app.py
+++ app.py
@@ -29,29 +29,6 @@ def check_to_changes():
         notebook.tab("current", text=name[:-1])
 
 
-def confirm_quiting():
-    unsaved = False
-
-    for tab in notebook.tabs():
-        current = root.nametowidget(tab)
-        content = current.get("1.0", "end-1c")
-
-        if hash(content) != contents[str(current)]:
-            unsaved = True
-            break
-
-    if unsaved == True:
-        confirm = messagebox.askyesno(
-            message="Changes to your file has not been saved . Do you want to quit ?",
-            icon="question",
-            title="Confirm Quit"
-        )
-        if not confirm:
-            return
-
-    root.destroy()
-
-
 def save_file():
     file = filedialog.asksaveasfilename()
     try:
@@ -82,6 +59,57 @@ def open_file():
     create_file(content, file_name)
 
 
+def quit_tab():
+    current = root.nametowidget(notebook.select())
+    if current_tab_unsaved() and not confirm_close():
+        return
+
+    notebook.forget(current)
+
+    if len(notebook.tabs()) == 0:
+        create_file()
+
+
+def current_tab_unsaved():
+    text_widget = root.nametowidget(notebook.select())
+    content = text_widget.get("1.0", "end-1c")
+
+    return hash(content) != contents[str(text_widget)]
+
+
+def confirm_close():
+    return messagebox.askyesno(
+        title="confirm close ?",
+        icon="question",
+        message="Changes to your file has not been saved . Do you want to close ?",
+    )
+
+
+def confirm_quit():
+    unsaved = False
+
+    for tab in notebook.tabs():
+        current = root.nametowidget(tab)
+        content = current.get("1.0", "end-1c")
+
+        if hash(content) != contents[str(current)]:
+            unsaved = True
+            break
+
+    if unsaved and not confirm_close():
+        return
+
+    root.destroy()
+
+
+def show_info():
+    messagebox.showinfo(
+        title="info",
+        message="This is a simple text editor created using tkinter . Thank you for using it .",
+        icon="info"
+    )
+
+
 root = tk.Tk()
 root.title('notebook')
 root.option_add("*tearOff", False)
@@ -93,12 +121,17 @@ menubar = tk.Menu()
 root.config(menu=menubar)
 
 file_menu = tk.Menu(menubar)
+help_menu = tk.Menu(menubar)
 menubar.add_cascade(label="File", menu=file_menu)
+menubar.add_cascade(label="Help", menu=help_menu)
 
 file_menu.add_command(label="New", command=create_file, accelerator="Ctrl+N")
 file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
 file_menu.add_command(label="Open", command=open_file, accelerator="Ctrl+O")
-file_menu.add_command(label="Quit", command=confirm_quiting, accelerator="Ctrl+Q")
+file_menu.add_command(label="Quit", command=quit_tab, accelerator="Ctrl+Q")
+file_menu.add_command(label="Exit", command=confirm_quit)
+
+help_menu.add_command(label="About", command=show_info)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
@@ -109,6 +142,6 @@ root.bind("<KeyPress>", lambda event: check_to_changes())
 root.bind("<Control-n>", lambda event: create_file())
 root.bind("<Control-s>", lambda event: save_file())
 root.bind("<Control-o>", lambda event: open_file())
-root.bind("<Control-q>", lambda event: confirm_quiting())
+root.bind("<Control-q>", lambda event: quit_tab())
 
 root.mainloop()
